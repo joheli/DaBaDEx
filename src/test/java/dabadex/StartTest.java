4fd74ffb3e547063b5e9e46627825f44c8d44a66
Johannes Elias <joheli@gmx.net>
2020-06-29 07:30:07 +0000

    make alterations to successfully build with gradle

--- src/test/java/dabadex/StartTest.java
+++ src/test/java/dabadex/StartTest.java
@@ -5,10 +5,11 @@ package dabadex;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.IOException;
 
 public class StartTest {
-    @Test public void testStartHasALogger() {
-        App classUnderTest = new Start();
+    @Test public void testStartHasALogger() throws SecurityException, IOException {
+        Start classUnderTest = new Start();
         assertNotNull("Start should have a logger", classUnderTest.LOGGER);
     }
 }
