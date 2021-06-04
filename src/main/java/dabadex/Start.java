c37ebcab15bc765afa5f817f627e1cb16286b5bc
Johannes Elias <joheli@gmx.net>
2021-06-04 09:51:10 +0000

    changed log levels for DBDX_LOGLEVEL

--- src/main/java/dabadex/Start.java
+++ src/main/java/dabadex/Start.java
@@ -36,12 +36,12 @@ public class Start implements Runnable {
 	  if (lvl_e != null) {
 	    try {
 	      lvl = Level.parse(lvl_e);
-	      LOGGER.log(Level.INFO, "'DBDX_LOGLEVEL' was set to " + lvl_e + ".");
+	      LOGGER.log(Level.CONFIG, "'DBDX_LOGLEVEL' was set to " + lvl_e + ".");
 	    } catch (Exception e) {
-	      LOGGER.log(Level.INFO, "Content of 'DBDX_LOGLEVEL' (" + lvl_e + ") could not be successfully translated to valid log level.");
+	      LOGGER.log(Level.WARNING, "Content of 'DBDX_LOGLEVEL' (" + lvl_e + ") could not be successfully translated to valid log level.");
 	    } // if it doesn't work don't bother further.
 	  } else {
-	    LOGGER.log(Level.INFO, "Environment variable 'DBDX_LOGLEVEL' was not set. Using default level.");
+	    LOGGER.log(Level.CONFIG, "Environment variable 'DBDX_LOGLEVEL' was not set. Using default level.");
 	  }
 	  return lvl;
 	}
